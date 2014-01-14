57bbbac47a114a32ff3337d00c25fd7f55922f3a
Dylan Foster <turtlecannon@gmail.com>
2014-01-14 11:42:55 -0800

    Slight changes for 13.04

--- emacs.d.install/init.el
+++ emacs.d.install/init.el
@@ -13,15 +13,15 @@
 
 ;; Tell emacs where to find the rosemacs sources
 ;; replace the path with location of rosemacs on your system
-(push "~/rosemacs" load-path) 
+;; (push "~/rosemacs" load-path) 
 
-;; Load the library and start it up
-(require 'rosemacs)
-(invoke-rosemacs)
+;; ;; Load the library and start it up
+;; (require 'rosemacs)
+;; (invoke-rosemacs)
 
 ;; Optional but highly recommended: add a prefix for quick access
 ;; to the rosemacs commands
-(global-set-key "\C-x\C-r" ros-keymap)
+;; (global-set-key "\C-x\C-r" ros-keymap)
 
 ;; Adds yaml mode
 (require 'yaml-mode)
